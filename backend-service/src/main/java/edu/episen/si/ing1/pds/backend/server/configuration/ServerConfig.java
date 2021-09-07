ba28a68f2b21bda9954935fd9112154cab5278a6
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-07 21:22:50 +0200

    Creation of serverConfig

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/configuration/ServerConfig.java
@@ -0,0 +1,29 @@
+package edu.episen.si.ing1.pds.backend.server.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ServerConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ServerConfig.class.getName());
+    private static final String episenServerConfigEnvVar = "EPISEN_SRV_CONFIG"; //environement variable name
+    private final String episenServerConfigFileLocation;
+    private ServerCoreConfig config;
+
+    public ServerConfig() throws IOException{
+        episenServerConfigFileLocation = System.getenv(episenServerConfigEnvVar);
+        logger.info("Config file = {}",episenServerConfigFileLocation);
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        config = mapper.readValue(new File(episenServerConfigFileLocation), ServerCoreConfig.class);
+        logger.info("config = {}", config.toString());
+    }
+    public ServerCoreConfig getConfig()
+    {
+        return config;
+    }
+
+}
