04a5c32bb3cbc918534b1d839c8fc8e6cf914831
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-12 21:01:52 +0200

    Creation of SocketFactory  class

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/socket/SocketFactory.java
@@ -0,0 +1,24 @@
+package edu.episen.si.ing1.pds.client.socket;
+
+import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
+import sun.jvm.hotspot.oops.Instance;
+
+import java.net.Socket;
+
+public enum SocketFactory {
+    Instance;
+    private Socket socket;
+
+    SocketFactory() {
+        try {
+            final ClientConfig clientConfig = new ClientConfig();
+            socket = new Socket(clientConfig.getConfig().getIpAddress(), clientConfig.getConfig().getListenPort());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+}
