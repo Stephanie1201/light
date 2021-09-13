4c7a22c66aa32e2e0841d0b29efbd791c6e56232
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-13 22:26:43 +0200

    Creation of ClientConfig class

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/configuration/ClientConfig.java
@@ -0,0 +1,27 @@
+package edu.episen.si.ing1.pds.client.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClientConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ClientConfig.class.getName());
+    private static final String episenClientConfigEnVar="CLIENT_CONFIG";
+    private String episenClientConfigFileLocation;
+    private ClientCoreConfig config;
+
+    public ClientConfig() throws IOException{
+        episenClientConfigFileLocation = System.getenv(episenClientConfigEnVar);
+        logger.info("Config file = {}", episenClientConfigFileLocation);
+        final ObjectMapper mapper = new ObjectMapper((new YAMLFactory()));
+        config = mapper.readValue(new File(episenClientConfigFileLocation),ClientCoreConfig.class);
+        logger.info("config = {}",config.toString());
+    }
+    public ClientCoreConfig getConfig(){
+        return  config;
+    }
+}
