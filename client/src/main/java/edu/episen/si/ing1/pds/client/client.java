94b11036e9221d24e34f8ba31c1389c0b1770e8d
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-13 22:26:24 +0200

    Creation of Client class

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/client.java
@@ -0,0 +1,43 @@
+package edu.episen.si.ing1.pds.client;
+
+import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
+import edu.episen.si.ing1.pds.client.view.HomePageView;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class client {
+    private static final Logger clientLogger = LoggerFactory.getLogger(client.class.getName()); // we don't use classic logger
+    public static ClientConfig clientConfig;
+
+    public static void main(String[] args) throws Exception{
+        final Options options = new Options();
+        final Option testMode = Option.builder().longOpt("testMode").build();
+        final Option clientMode = Option.builder().longOpt("clientMode").build();
+        final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().argName("maxConnection").build();
+        int maxConnectionV = 5;
+
+        options.addOption(testMode);
+        options.addOption(clientMode);
+        options.addOption(maxConnection);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        boolean inTestMode = false;
+        if (commandLine.hasOption("testMode")) {
+            inTestMode = true;
+        }
+        if (commandLine.hasOption("maxConnection")){
+            maxConnectionV = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
+        }
+
+        clientConfig = new ClientConfig();
+        if (commandLine.hasOption("clientMode")){
+            HomePageView hm = new HomePageView();
+            hm.setVisible(true);
+        }
+        clientLogger.info("Client is running (testMode={}).",inTestMode);
+
+    }
+}
