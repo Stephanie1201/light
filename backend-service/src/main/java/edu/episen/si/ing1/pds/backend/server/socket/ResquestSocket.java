84f856eb6742cf3efa9ceefdd6b13533b21ca28f
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-13 22:28:58 +0200

    Creation of Request and Response class

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/socket/ResquestSocket.java
@@ -0,0 +1,12 @@
+package edu.episen.si.ing1.pds.backend.server.socket;
+
+public class ResquestSocket {
+    private String request;
+    private Object data;
+
+    public String getRequest(){return request;}
+    public void setRequest(String request){this.request = request;}
+    public Object getData() {return data;}
+    public void setData (Object data) { this.data = data;}
+
+}
---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/socket/ResponseSocket.java
@@ -0,0 +1,26 @@
+package edu.episen.si.ing1.pds.client.socket;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+public class ResponseSocket {
+    private String request;
+    private Object data;
+
+    @JsonGetter("request")
+    public String getRequest(){return request;}
+    public Object getData(){return data; }
+
+    @JsonSetter("request")
+    public void setRequest(String request){ this.request = request;}
+
+    public void setData(Object data){this.data = data;}
+
+    @Override
+    public String toString(){
+        return "ResponseSocket{" +
+                "request='" + request + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
