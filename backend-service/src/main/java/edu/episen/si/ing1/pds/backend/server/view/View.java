25c5e0fbbe4bb6b4e6cdc283c30fe67160f3fe8b
Stephanie Ndongmo <nguimdofany@mail.com>
2021-09-21 21:39:11 +0200

    creation of view

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/view/View.java
@@ -0,0 +1,14 @@
+package edu.episen.si.ing1.pds.backend.server.view;
+
+public class View {
+
+    public void viewShow() {
+        System.out.println("1-- Press s + enter if you want to display the Student table \n");
+        System.out.println("2-- Press i + enter if you want to insert firstname and lastname in the Student table \n");
+        System.out.println("3-- Press d + enter if you want to delete line(s) in the Student table \n");
+        System.out.println("4-- Press u + enter if you want to update lines(s) in the Student table \n");
+        System.out.println("5-- Press q + enter if you want to quit \n");
+    }
+
+
+}
