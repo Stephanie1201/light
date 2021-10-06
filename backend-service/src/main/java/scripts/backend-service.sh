178beafd5bcc94b3851ed3ead225ba40dc4f6066
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:04:56 +0200

    Modification of backend

--- backend-service/src/main/java/scripts/backend-service.sh
+++ backend-service/src/main/java/scripts/backend-service.sh
@@ -1,2 +1,1 @@
-
-java -cp ${M2_REPO}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar episen.si.ing1.pds.backend.server.BackendService $*
+java -cp ${M2_REPO}/edu/episen/si/ing1/pds/backend-service/1.0-SNAPSHOT/backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar edu.episen.si.ing1.pds.backend.server.BackendService $*
