4e7556fc310017371707a4dbbb40bdb5d80ae81a
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:13:24 +0200

    Modification of backendService script

--- backend-service/src/main/java/scripts/backend-service.bat
+++ backend-service/src/main/java/scripts/backend-service.bat
@@ -1,3 +1,3 @@
-set m2=%M2_REPO%
+set m2=C:\Users\nguim\.m2\repository
 set classpath=%m2%\edu\episen\si\ing1\pds\backend-service\1.0-SNAPSHOT\backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
-call java -cp %classpath% BackendService %*
+call java -cp %classpath% edu.episen.si.ing1.pds.backend.server.BackendService %*
