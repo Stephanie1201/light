eb6724533f59e4b1571e8839c4d8bc570f908fe8
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:15:38 +0200

    modification of deploiement scripts

--- scripts/deploiementofbackend.bat
+++ scripts/deploiementofbackend.bat
@@ -1,5 +1,6 @@
 set m2=C:\Users\nguim\.m2\repository\edu\episen\si\ing1\pds\backend-service\1.0-SNAPSHOT\backend-service-1.0-SNAPSHOT-jar-with-dependencies.jar
-set project=C:\Users\nguim\IdeaProjects\light\backend-service\src\main\java\scripts\backend-service.sh
-scp %m2% toto@172.31.249.106:/home/toto
-scp %project% toto@172.31.249.106:/home/toto/demo/
-ssh toto@172.31.249.106 chmod 777 /home/toto/demo/backend-service.sh
+set project=C:\Users\nguim\IdeaProjects\light\backend-service\src\main\java\scripts\server.sh
+scp %m2% admin@172.31.249.106:/usr/local/light/jar
+scp %project% admin@172.31.249.106:/usr/local/light/scripts
+ssh admin@172.31.249.106  chmod 777 /usr/local/light/scripts/server.sh
+
