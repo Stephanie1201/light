80665f0c2229fdc99a7e394a86c36ad87cddbe87
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:11:06 +0200

    Modification of JDBCConnection

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/pool/JDBCConnectionPool.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/pool/JDBCConnectionPool.java
@@ -1,14 +1,15 @@
 package edu.episen.si.ing1.pds.backend.server.pool;
 
 import edu.episen.si.ing1.pds.backend.server.configuration.DatabaseConfig;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.logging.Logger;
 
 //this class represent all of physical connexion
 public class JDBCConnectionPool {
@@ -20,7 +21,7 @@ public class JDBCConnectionPool {
     private int maxConnection;
     private static boolean isInit = false;
     private   DatabaseConfig databaseConfig;
-    //private static final Logger logger = LoggerFactory.getLogger(JDBCConnectionPool.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(JDBCConnectionPool.class.getName());
 
     //this methode use propertiesReader to take the DB parameter
     public JDBCConnectionPool() {
@@ -31,6 +32,7 @@ public class JDBCConnectionPool {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Que fais ces instructions
          driverName = databaseConfig.getConfig().getDRIVER_NAME();
          dataBaseUrl = databaseConfig.getConfig().getDATABASE_URL();
          user = databaseConfig.getConfig().getUSER();
@@ -39,7 +41,22 @@ public class JDBCConnectionPool {
          physicalConnections = new ArrayList<>();
 
     }
-
+public void init(){
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int i=0; i< maxConnection; i++){
+            Connection connection = null;
+            try{
+                connection = DriverManager.getConnection(dataBaseUrl,user,passWord);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            physicalConnections.add(connection);
+        }
+}
 public Connection getConnection(){
         return physicalConnections.remove(0);
 }
@@ -66,6 +83,7 @@ public int getSizeArrayConnection(){
     }
 //return true if there is no more connection in the pool
     public boolean isEmpty(){
+        // isEmty= est vide
         return physicalConnections.size() == 0;
     }
 }
