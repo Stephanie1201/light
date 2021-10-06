d26e76c727af8156a01aa40d4b2cf36750d644c2
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:14:59 +0200

    Modification of client script

--- client/src/main/java/scripts/client.bat
+++ client/src/main/java/scripts/client.bat
@@ -1,3 +1,3 @@
 set m2=%M2_REPO%
 set clsspth=%m2%\edu\episen\si\ing1\pds\client\1.0-SNAPSHOT\client-1.0-SNAPSHOT-jar-with-dependencies.jar
-call java -cp %clsspth% episen.si.ing1.pds.client.view.WelcomeFrame %*
+call java -cp %clsspth% edu.episen.si.ing1.pds.client.client %*
