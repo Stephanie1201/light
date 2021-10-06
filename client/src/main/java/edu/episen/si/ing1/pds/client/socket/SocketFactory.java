54590aac472f1178b88241f895cbe3746bcb1493
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-06 14:13:02 +0200

    Modification of SocketFactory

--- client/src/main/java/edu/episen/si/ing1/pds/client/socket/SocketFactory.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/socket/SocketFactory.java
@@ -1,14 +1,14 @@
 package edu.episen.si.ing1.pds.client.socket;
 
 import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
-import sun.jvm.hotspot.oops.Instance;
+
 
 import java.net.Socket;
 
 public enum SocketFactory {
     Instance;
     private Socket socket;
-
+// This class create socket
     SocketFactory() {
         try {
             final ClientConfig clientConfig = new ClientConfig();
