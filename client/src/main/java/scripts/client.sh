ebb935d7aff9fc581d8fb3668ba9bbdd326f6127
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-08 00:15:26 +0200

    Modification of Client script

--- client/src/main/java/scripts/client.sh
+++ client/src/main/java/scripts/client.sh
@@ -1,3 +1,3 @@
 m2={M2_REPO}
 clsspth= ${m2}/edu/episen/si/ing1/pds/client/1.0-SNAPSHOT/client-1.0-SNAPSHOT-jar-with-dependencies.jar
-exec java -cp ${clsspth} edu.episen.si.ing1.pds.client.client $*
+exec java -cp ${clsspth} edu.episen.si.ing1.pds.client.view.HomePageView $*
