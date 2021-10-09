c30f1165b8087188d28df814061a5c8c13b1fae7
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-09 20:49:27 +0200

    Modification of clientResquest class

--- client/src/main/java/edu/episen/si/ing1/pds/client/ClientRequest.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/ClientRequest.java
@@ -3,6 +3,7 @@ package edu.episen.si.ing1.pds.client;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
+import edu.episen.si.ing1.pds.client.socket.RequestSocket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.File;
@@ -21,26 +22,37 @@ public class ClientRequest {
     private static String clientDataEnvVar = "CLIENT_DATA_JSON";
 
     public ClientRequest(final ClientConfig config) throws IOException{
-        clientSocket = new Socket(config.getConfig().getIpAddress(), config.getConfig().getListenPort());
+        clientSocket = new Socket("localhost",  config.getConfig().getListenPort());
+        //clientSocket = new Socket(config.getConfig().getIpAddress(), config.getConfig().getListenPort());
         clientDataFileLocation = System.getenv(clientDataEnvVar);
     }
 
-    public void startConnection() throws IOException, ClassNotFoundException, InterruptedException{
+    public void startConnection(String requestName) throws IOException, ClassNotFoundException, InterruptedException {
+        try{
         outputStream = clientSocket.getOutputStream();
         inputStream = clientSocket.getInputStream();
         byte[] inputData;
 
         final ObjectMapper mapper = new ObjectMapper(new JsonFactory());
-        Student mapJsonFile = mapper.readValue(new File(clientDataFileLocation), Student.class);
+        String path= clientDataFileLocation + "/"+requestName+".json";
+        RequestSocket mapJsonFile = mapper.readValue(new File(path), RequestSocket.class);
         outputStream.write(mapper.writeValueAsBytes(mapJsonFile)); //envoie au format json
+        outputStream.flush();//assure l'envoie
+           // outputStream.close();
+        logger.info(mapJsonFile.toString());
         logger.info(("Request submitted"));
 
-        while (inputStream.available() == 0){}
+        while (inputStream.available() == 0) {
 
+        }
         inputData = new byte[inputStream.available()];
         inputStream.read(inputData);
         String serverResponse = new String(inputData);
-        System.out.println(formatString(serverResponse));
+        logger.info(formatString(serverResponse));
+    }catch(Exception ex)
+    {
+        ex.printStackTrace();
+    }
     }
     public void stopConnection() throws IOException {
         clientSocket.close();
