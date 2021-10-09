2c1dcf517f540bd950e15c0cbd92064bd49ee0f7
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-09 20:48:38 +0200

    Modification of client class

--- client/src/main/java/edu/episen/si/ing1/pds/client/client.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/client.java
@@ -15,8 +15,9 @@ public class client {
         final Option testMode = Option.builder().longOpt("testMode").build();
         final Option clientMode = Option.builder().longOpt("clientMode").build();
         final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().argName("maxConnection").build();
+        final Option requestName = Option.builder().longOpt("request").hasArg().argName("request").build();
         int maxConnectionV = 5;
-
+        options.addOption(requestName);
         options.addOption(testMode);
         options.addOption(clientMode);
         options.addOption(maxConnection);
@@ -28,8 +29,8 @@ public class client {
         boolean inTestMode = false;
         if (commandLine.hasOption("testMode")) {
             ClientRequest clientRequest = new ClientRequest(clientConfig);
-            clientRequest.startConnection();
-            clientRequest.stopConnection();
+            clientRequest.startConnection((String)commandLine.getOptionValue("request"));//Donner a la méthode la valeur de ta requête
+           //clientRequest.stopConnection();
         }
         if (commandLine.hasOption("maxConnection")){
             maxConnectionV = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
