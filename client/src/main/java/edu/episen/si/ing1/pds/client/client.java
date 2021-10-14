d62e3d62d801bcbdc078f04468057981a8972e6d
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-14 17:21:19 +0200

    Modification of client to add the max connection argument and the number of client who can get connection

--- client/src/main/java/edu/episen/si/ing1/pds/client/client.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/client.java
@@ -1,27 +1,31 @@
 package edu.episen.si.ing1.pds.client;
 
+import ch.qos.logback.core.net.server.Client;
 import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
 import edu.episen.si.ing1.pds.client.view.HomePageView;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+
 public class client {
     private static final Logger clientLogger = LoggerFactory.getLogger(client.class.getName()); // we don't use classic logger
-    public static ClientConfig clientConfig;
 
+ public static ClientConfig clientConfig;
     public static void main(String[] args) throws Exception{
         final Options options = new Options();
         final Option testMode = Option.builder().longOpt("testMode").build();
         final Option clientMode = Option.builder().longOpt("clientMode").build();
-        final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().argName("maxConnection").build();
         final Option requestName = Option.builder().longOpt("request").hasArg().argName("request").build();
-        int maxConnectionV = 5;
+        final Option testMode2 = Option.builder().longOpt("testMode2").build();
+        final Option numberClients = Option.builder().longOpt("numberClient").hasArg().argName("numberClient").build();
+
         options.addOption(requestName);
         options.addOption(testMode);
         options.addOption(clientMode);
-        options.addOption(maxConnection);
-
+        options.addOption(testMode2);
+        options.addOption(numberClients);
         final CommandLineParser parser = new DefaultParser();
         final CommandLine commandLine = parser.parse(options, args);
 
@@ -32,15 +36,31 @@ public class client {
             clientRequest.startConnection((String)commandLine.getOptionValue("request"));//give to the methode the value of request
            //clientRequest.stopConnection();
         }
-        if (commandLine.hasOption("maxConnection")){
-            maxConnectionV = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
+        if (commandLine.hasOption("testMode2") && commandLine.hasOption("numberClient")) {
+            ArrayList<Thread> clientThreads=new ArrayList<>();
+            int numberClientValue = Integer.parseInt(commandLine.getOptionValue("numberClient"));
+
+            for(int i=1;i<=numberClientValue;i++){
+                ClientThread client=new ClientThread(clientConfig,"Client "+i); //Je crée les clients et les rajoute dans la array liste
+                clientThreads.add(client);
+            }
+            //Une fois tous les clients rajoutés, je les demarre tous
+            for (Thread client: clientThreads
+                 ) {
+                client.start();
+
+            }
         }
 
         if (commandLine.hasOption("clientMode")){
             HomePageView hm = new HomePageView();
             hm.setVisible(true);
         }
-        clientLogger.info("Client is running (testMode={}).",inTestMode);
+
+
+
+
+        //clientLogger.info("Client is running (testMode={}).",inTestMode);
 
     }
 }
