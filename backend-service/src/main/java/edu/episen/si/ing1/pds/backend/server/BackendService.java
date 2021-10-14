9f2ed35842b0e5e72cfb8501b1ebfe5d8456b86b
Stephanie Ndongmo <nguimdofany@mail.com>
2021-10-14 17:19:52 +0200

    Modification of BackendService to add the max connection argument

--- backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/BackendService.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/backend/server/BackendService.java
@@ -15,25 +15,32 @@ public class BackendService {
         final Option testMode = Option.builder().longOpt("testMode").build(); // use longOpt to write --testMode in cmd
         final Option testModeT = Option.builder().longOpt("testModeT").build();
         final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().argName("maxConnection").build();
+
         options.addOption(testMode);  // add the "testMode" option to your options
         options.addOption(maxConnection);
         options.addOption(testModeT);
         final CommandLineParser parser = new DefaultParser();
         final CommandLine commandLine = parser.parse(options, args);
-        int maxConnectionV = 10;
+        int maxConnectionV = 5;
         boolean inTestMode = false;
         boolean inTestModeT = false;
+        if (commandLine.hasOption("maxConnection")) {
+
+            maxConnectionV = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
+        }
+
         if (commandLine.hasOption("testMode")) {
             inTestMode = true;
             ServerConfig serverConfig=new ServerConfig();//Read the yaml file server for the configuration
+
+
             DataSource ds = DataSource.getInstance();//For initialisation of the connexion pool
+            ds.startPool(maxConnectionV);
             ServerCore serverCore =new ServerCore(serverConfig,ds); //Create the serverSocket
             serverCore.serve(); // manage the client connexion
             ds.closePool();
         }
-        if (commandLine.hasOption("maxConnection")) {
-            maxConnectionV = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
-        }
+
 
         serverLogger.info("yes! BackendService is running (testMode={}), (testModeT={}), (maxConnection={}). ", inTestMode, inTestModeT, maxConnectionV);
 
