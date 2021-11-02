3232e137cb73c70a08d35db92730d5f33368c416
Stephanie Ndongmo <nguimdofany@mail.com>
2021-11-02 18:50:42 +0100

    Modication of SocketFactory

--- client/src/main/java/edu/episen/si/ing1/pds/client/socket/SocketFactory.java
+++ client/src/main/java/edu/episen/si/ing1/pds/client/socket/SocketFactory.java
@@ -1,18 +1,16 @@
 package edu.episen.si.ing1.pds.client.socket;
 
 import edu.episen.si.ing1.pds.client.configuration.ClientConfig;
-
-
 import java.net.Socket;
 
 public enum SocketFactory {
     Instance;
     private Socket socket;
-// This class create socket
+
     SocketFactory() {
         try {
             final ClientConfig clientConfig = new ClientConfig();
-            socket = new Socket(clientConfig.getConfig().getIpAddress(), clientConfig.getConfig().getListenPort());
+            socket = new  Socket(clientConfig.getConfig().getIpAddress(), clientConfig.getConfig().getListenPort());
         } catch (Exception e) {
             e.printStackTrace();
         }
