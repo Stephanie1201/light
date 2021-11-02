fd274392abb4f40cc61fa9e4a5f45ff92e55ead3
Stephanie Ndongmo <nguimdofany@mail.com>
2021-11-02 18:55:36 +0100

    Modification of Datatemp

---
+++ client/src/main/java/edu/episen/si/ing1/pds/client/model/Datatemp.java
@@ -0,0 +1,15 @@
+package edu.episen.si.ing1.pds.client.model;
+
+public class Datatemp {
+    private static int id_datatemp=1;
+
+
+    public static int getid_datatemp() {
+        return id_datatemp;
+    }
+
+    public static void setid_datatemp(int id_datatemp) {
+        Datatemp.id_datatemp = id_datatemp;
+    }
+
+}
